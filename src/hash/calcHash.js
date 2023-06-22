8c9f3c7554617b7b124cc8a2f2f83ecb9ebc385d
Bulrock <maksim.volozhinskij@gmail.com>
2023-06-22 23:01:11 +0200

    feat: implement hash block

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import fs from 'fs/promises';
+import crypto from 'crypto';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = path.resolve(__dirname, 'files', 'fileToCalculateHashFor.txt');
+    
+    try {
+        const secret = await fs.readFile(filePath, {encoding: 'utf8'})
+        const hash = crypto.createHmac('sha256', secret)
+                   .digest('hex');
+        console.log(hash);
+    } catch (err){
+        console.log(err);
+    }
 };
 
 await calculateHash();
