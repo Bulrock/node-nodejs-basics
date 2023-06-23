ed0825c4800dca2fffd8660fdfaa5ab5360a9e4d
Bulrock <maksim.volozhinskij@gmail.com>
2023-06-23 16:55:01 +0200

    feat: implement zip block

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import { createGzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import path from 'path';
+import { pipeline } from 'stream';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+    const fileToCompressName = 'fileToCompress.txt';
+    const compressedFileName = 'archive.gz';
+    const fileToCompressPath = path.resolve(__dirname, 'files', fileToCompressName)
+    const compressedFilePath = path.resolve(__dirname, 'files', compressedFileName)
+
+    pipeline(
+        createReadStream(fileToCompressPath),
+        createGzip(),
+        createWriteStream(compressedFilePath),
+        (err) => console.error(err)
+    )
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import { createGunzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import path from 'path';
+import { pipeline } from 'stream';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+    const fileToDecompressName = 'archive.gz';
+    const DecompressedFileName = 'DecompressedFile.txt';
+    const fileToDecompressPath = path.resolve(__dirname, 'files', fileToDecompressName)
+    const DecompressedFilePath = path.resolve(__dirname, 'files', DecompressedFileName)
+
+    pipeline(
+        createReadStream(fileToDecompressPath),
+        createGunzip(),
+        createWriteStream(DecompressedFilePath),
+        (err) => console.error(err)
+    )
 };
 
 await decompress();
