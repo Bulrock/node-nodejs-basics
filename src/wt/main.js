4f8c7a7136624f46626c71a696f10ca9b094f6fa
Bulrock <maksim.volozhinskij@gmail.com>
2023-06-24 12:58:18 +0200

    feat: implement wt block

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,33 @@
+import { Worker } from 'worker_threads';
+import { availableParallelism } from 'os';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const INITIAL_NUMBER = 10;
+const workerPath = path.resolve(__dirname, 'worker.js');
+
+const fibonacciWorkerService = (number) => new Promise (resolve => {
+    const worker = new Worker(workerPath, {workerData: number});
+
+    worker.on('message', data => resolve({
+        status: 'resolved',
+        data
+    }));
+
+    worker.on('error', data => resolve({
+        status: 'error',
+        data: null
+    }));
+})
+
 const performCalculations = async () => {
-    // Write your code here
+    const workersPool = Array.from({length: availableParallelism()}, (_, i) => fibonacciWorkerService(INITIAL_NUMBER + i))
+    const result = await Promise.all(workersPool);
+    console.log(result);
 };
 
 await performCalculations();
